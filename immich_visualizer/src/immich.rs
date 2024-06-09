66be9709651f9a7e0cbd4c2336c2a771e72672be
Antoine <antoinelassagne@gmail.com>
2024-06-09 12:06:45 +0200

    WIP

--- immich_visualizer/src/immich.rs
+++ immich_visualizer/src/immich.rs
@@ -1,10 +1,11 @@
 extern crate openapi;
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+// use ferris_says::say; // from the previous step
+// use std::io::{stdout, BufWriter};
 
 use openapi::apis::server_info_api::ping_server; // Replace with the actual module paths
 use openapi::apis::assets_api::get_all_assets; // Replace with the actual module paths
 use openapi::apis::configuration::{ApiKey, Configuration};   // Replace with the actual module paths
+use openapi::models;
 
 pub struct ApiClient {
     config: Configuration,
@@ -23,27 +24,27 @@ impl ApiClient {
     }
 
     // Method to set the base URL
-    pub fn set_base_url(&mut self, base_url: String) {
-        self.config.base_path = base_url;
-    }
+    // pub fn set_base_url(&mut self, base_url: String) {
+    //     self.config.base_path = base_url;
+    // }
 
-    // Method to get the base URL
-    pub fn get_base_url(&self) -> &str {
-        &self.config.base_path
-    }
+    // // Method to get the base URL
+    // pub fn get_base_url(&self) -> &str {
+    //     &self.config.base_path
+    // }
 
-    pub async fn get_all_assets(&self) -> Result<String, String> {
+    pub async fn get_all_assets(&self) -> Result<Vec<models::AssetResponseDto>, String> {
         // Simulate an API call
         if self.config.base_path.is_empty() {
             return Err("Base URL is empty".to_string())
         }
 
-        let message = match get_all_assets(&self.config).await {
+        let message: Result<Vec<models::AssetResponseDto>, String> = match get_all_assets(&self.config).await {
             Ok(response) => {
                 if self.verbose {
                     println!("Ping response: {:?}", response);
                 }
-                Ok(String::from("Immich connected."))
+                Ok(response)
             },
             Err(e) => {
                 if self.verbose {
@@ -54,7 +55,6 @@ impl ApiClient {
         };
 
         message
-
     }
 
     // Example method to simulate an API call
--- immich_visualizer/src/main.rs
+++ immich_visualizer/src/main.rs
@@ -1,9 +1,15 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+// use ferris_says::say; // from the previous step
+// use std::io::{stdout, BufWriter};
 use std::process::exit;
 use std::env;
+use openapi::models;
+use rand::Rng;
+use chrono::Utc;
+use chrono::prelude::*; // Import the prelude to get the `Datelike` trait
+
 mod immich;
 
+extern crate openapi;
 #[tokio::main]
 async fn main() {
 
@@ -21,8 +27,10 @@ async fn main() {
         },
     };
 
-    let mut client = immich::ApiClient::new("http://192.168.50.214:2283/api".to_string(), api_key.to_string(), true);
-    let res = match client.ping().await{
+    let client = immich::ApiClient::new("http://192.168.50.214:2283/api".to_string(),
+        api_key.to_string(),
+        false);
+    let _res = match client.ping().await{
         Ok(response) => {
             println!("Ping response: {:?}", response);
             Ok(String::from("Immich connected."))
@@ -33,16 +41,20 @@ async fn main() {
         },
     };
 
-    let res = match client.get_all_assets().await{
-        Ok(response) => {
-            println!("Ping response: {:?}", response);
-            Ok(String::from("Immich connected."))
-        },
-        Err(e) => {
-            eprintln!("Error calling /server-info/ping: {:?}", e);
-            Err(String::from("Aaaaarrrh!"))
-        },
-    };
+    eprintln!("Retrieving all assets...");
+    let all_assets: Vec<models::AssetResponseDto> = client.get_all_assets().await.expect("F***");
+
+    // Generate a random year between 1992 and the current year
+    let current_year = Utc::now().year();
+    let year = rand::thread_rng().gen_range(2012..=current_year);
+
+    let filtered_assets: Vec<&models::AssetResponseDto> = all_assets.iter().filter(|&asset| {
+        // eprintln!("YEAR: {}", asset.local_date_time);
+        asset.file_created_at.starts_with(&year.to_string())
+    }).collect();
+
+    eprintln!("{} assets retrieved in year {}", filtered_assets.len(), year);
+
 
     MainWindow::new().unwrap().run().unwrap();
 }
