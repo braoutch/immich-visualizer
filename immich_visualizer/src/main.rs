f8661dec528b3c4c642966bcdc86f91091b33cb7
Antoine <antoinelassagne@gmail.com>
2024-06-22 20:57:08 +0200

    v0.1

--- immich_visualizer/src/main.rs
+++ immich_visualizer/src/main.rs
@@ -13,6 +13,7 @@ use bytes::Bytes;
 use image::ImageFormat;
 use slint::SharedPixelBuffer;
 use slint::Rgba8Pixel;
+use std::time::Instant;
 
 mod immich;
 
@@ -147,20 +148,14 @@ async fn main() -> Result<(), slint::PlatformError> {
             
             let client = immich::ApiClient::new("http://192.168.50.214:2283/api".to_string(), api_key.to_string(), true);
             eprintln!("PING {}...", count);
-            let _res = match client.ping().await {
-                Ok(response) => {
-                    println!("Ping response: {:?}", response);
-                    Ok(String::from("Immich connected."))
-                },
+            let start_time = Instant::now();
+            let random_asset: Vec<models::AssetResponseDto> = match client.get_random_asset().await {
+                Ok(response) => response,
                 Err(e) => {
-                    eprintln!("Error calling /server-info/ping: {:?}", e);
-                    Err(String::from("Aaaaarrrh!"))
+                    eprintln!("Error retrieving random asset: {:?}. Skipping.", e);
+                    continue;
                 },
             };
-
-            eprintln!("Retrieving a random asset...");
-
-            let random_asset: Vec<models::AssetResponseDto> = client.get_random_asset().await.expect("F***");
 
             // print the number of items in random_asset
             eprintln!("{} assets retrieved", random_asset.len());
@@ -174,38 +169,48 @@ async fn main() -> Result<(), slint::PlatformError> {
 
 
                 eprintln!("Asset: {:?}", asset.id.clone());
-                let _image: (Bytes, String) = client.download_image(asset.id.clone()).await.expect("Nooo");
-                // check the type of the image and, if it's not jpeg, continue the loop
-                let image_format = type_str_to_image_type(&_image.1);
-                if image_format != Some(ImageFormat::Jpeg) {
-                    eprintln!("Skip image format: {:?}", image_format);
-                    continue;
-                }
+                let _image: (Bytes, String) = match client.download_image(asset.id.clone()).await {
+                    Ok(response) => response,
+                    Err(e) => {
+                        eprintln!("Error downloading image: {:?}. Skipping.", e);
+                        continue;
+                    },
+                };
+                let elapsed_time = start_time.elapsed();
+                eprintln!("Download execution time: {:?}", elapsed_time);
 
-                // let _slint_image = bytes_to_image(_image.0, image_format.unwrap()).expect("Invalid image conversion");
-                let pixel_buffer = bytes_to_shared_image(_image.0, image_format.unwrap()).expect("Invalid image conversion");
+                // check the type of the image and, if it's not jpeg, continue the loop
+                let image_format = match type_str_to_image_type(&_image.1) {
+                    Some(ImageFormat::Jpeg) => Some(ImageFormat::Jpeg),
+                    Some(ImageFormat::Png) => Some(ImageFormat::Png),
+                    _ => { 
+                        // print the error
+                        eprintln!("Unsupported format {}", &_image.1);
+                        continue 
+                    },                 
+                };
+
+                let pixel_buffer = match bytes_to_shared_image(_image.0, image_format.unwrap()) {
+                    Ok(response) => response,
+                    Err(e) => {
+                        eprintln!("Error converting image: {:?}. Skipping.", e);
+                        continue;
+                    },
+                };
+
                 // let ui = ui_handle.unwrap();
                 let handle_copy = ui_handle.clone();
                 let _ = slint::invoke_from_event_loop(move || {
                     let image = slint::Image::from_rgba8_premultiplied(pixel_buffer);
                     handle_copy.unwrap().set_image_source(image);
                 });
+                // let handle_copy = ui_handle.clone();
                 // let _ = slint::invoke_from_event_loop(move || handle_copy.unwrap().set_image_text(String::from("Hello, world!").into()));
-                // ui.set_image_source(_slint_image);
                 break;
             }   
         
-
             std::thread::sleep(time::Duration::from_millis(2000));
             count += 1;
-            if count > 3 {
-                break;
-            }
-
-            // if Utc::now().timestamp() - switch_time > 5 {
-            //     switch_time = Utc::now().timestamp();
-            //     break;
-            // }
         }
      });
 
